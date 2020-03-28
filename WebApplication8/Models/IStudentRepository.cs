d46589550495f01dd5ed6b8d3295d1bbaabbd759
Joyce Liu <j.liu@mineware.com>
2020-03-28 10:58:15 +1000

    Add project files.

---
+++ WebApplication8/Models/IStudentRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication8.Models
+{
+    //Business Layer interface
+    public interface IStudentRepository
+    {
+        Student GetStudentById(int studentId);
+
+        public void Insert(Student student);
+
+        public void Update(Student student);
+
+        public void Delete(Student student);
+    }
+}
---
+++ WebApplication8/Models/TestStudentRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication8.Models
+{
+    //It is the business layer
+    public class TestStudentRepository : IStudentRepository
+    {
+        private List<Student> _students;
+
+        public TestStudentRepository()
+        {
+            _students = new List<Student>()
+            {
+               new Student() { StudentId = 101, Name = "James", Branch = "CSE", Section = "A", Gender = "Male" },
+               new Student() { StudentId = 102, Name = "Smith", Branch = "ETC", Section = "B", Gender = "Male" },
+               new Student() { StudentId = 103, Name = "David", Branch = "CSE", Section = "A", Gender = "Male" },
+               new Student() { StudentId = 104, Name = "Sara", Branch = "CSE", Section = "A", Gender = "Female" },
+            };
+        }
+
+        public void Delete(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetStudentById(int studentId)
+        {
+            return _students.FirstOrDefault(c => c.StudentId == studentId);
+        }
+
+        public void Insert(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
